8165225bbf4d3a6d1d8056d20a74106c184b60c6
Anton Shabelnyk <anton_shabelnyk@hotmail.com>
2018-08-02 09:08:04 +0300

    + recreated project

---
+++ S03_Building_Escape/Source/S03_Building_Escape/S03_Building_EscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "S03_Building_EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S03_BUILDING_ESCAPE_API AS03_Building_EscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
